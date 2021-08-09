aec9808eb81a94cf6789c9bfcffeb7baa05c7fbf
Andrea Pagnani <andrea.pagnani@gmail.com>
2021-08-09 19:34:05 +0200

    thread safe _outputarnet! + multithread epistatic score

--- src/types.jl
+++ src/types.jl
@@ -99,8 +99,8 @@ function _outputarnet( xs, J, H, p0, N, q)
 end
 
 let DtotH = Dict{Int,Vector{Float64}}()
-    global _outputarnet!
-    function _outputarnet!(dest, x, J, H, p0, N, q)
+    global _outputarnet_orig!
+    function _outputarnet_orig!(dest, x, J, H, p0, N, q)
         dest[1] = p0[x[1]]
         totH = Base.get!(DtotH,q,Vector{Float64}(undef,q))
         #fill!(totH,0.0)
@@ -120,6 +120,27 @@ let DtotH = Dict{Int,Vector{Float64}}()
     end
 end
 
+let DtotH = Dict{Tuple{Int,Int},Vector{Float64}}()
+    global _outputarnet!
+    function _outputarnet!(dest, x, J, H, p0, N, q)
+        dest[1] = p0[x[1]]
+        totH = Base.get!(DtotH,(q,Threads.threadid()),Vector{Float64}(undef,q))
+        #fill!(totH,0.0)
+        @inbounds for site in 1:N-1
+            Js = J[site]
+            h = H[site]
+            copy!(totH,h)
+            @avx for i in 1:site
+                for a in 1:q
+                    totH[a] += Js[a,x[i],i]
+                end
+            end
+            softmax!(totH)
+            dest[site+1]=totH[x[site+1]]
+        end
+        dest
+    end
+end
 
 
 
