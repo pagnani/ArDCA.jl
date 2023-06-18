cc73564f58ace10065f4547df7d84d7d3db690fb
Andrea Pagnani <andrea.pagnani@gmail.com>
2023-06-18 15:55:24 +0200

    using -> import

--- src/ArDCA.jl
+++ src/ArDCA.jl
@@ -1,14 +1,14 @@
 module ArDCA
 
-using Random: randperm 
-using Printf: @printf 
-using LinearAlgebra: rmul!, norm
-using ExtractMacro: @extract
-using NLopt: Opt,ftol_abs!,xtol_rel!,xtol_abs!,ftol_rel!,maxeval!,min_objective!,optimize
-using Distributions: wsample
-using LoopVectorization: @turbo
-using DCAUtils: read_fasta_alignment,remove_duplicate_sequences,compute_weights
-using DCAUtils.ReadFastaAlignment: letter2num
+import Random: randperm 
+import Printf: @printf 
+import LinearAlgebra: rmul!, norm
+import ExtractMacro: @extract
+import NLopt: Opt,ftol_abs!,xtol_rel!,xtol_abs!,ftol_rel!,maxeval!,min_objective!,optimize
+import Distributions: wsample
+import LoopVectorization: @turbo
+import DCAUtils: read_fasta_alignment,remove_duplicate_sequences,compute_weights
+import DCAUtils.ReadFastaAlignment: letter2num
 
 export ardca,ArVar,ArAlg,ArNet,sample,sample_with_weights,epistatic_score,dms_single_site,loglikelihood,siteloglikelihood
 
